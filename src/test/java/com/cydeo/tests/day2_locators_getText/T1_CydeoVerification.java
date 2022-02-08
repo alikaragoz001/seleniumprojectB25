d3ccf37b8a6b0292c097f457743e1cfcec0cd920
alikaragoz <karagozali2001@gmail.com>
2022-02-08 09:06:48 -0500

    day 2 selenium

--- src/test/java/com/cydeo/tests/day2_locators_getText/T1_CydeoVerification.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText/T1_CydeoVerification.java
@@ -11,6 +11,28 @@ public class T1_CydeoVerification {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.navigate().to("https://practice.cydeo.com");
+
+        //verification
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+
+        if (actualURL.contains(expectedURL)){
+            System.out.println("URL verification passed");
+        }else{
+            System.out.println("URL verification failed");
+        }
+
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title Verification Passed");
+        }else{
+            System.out.println("Title verification failed");
+        }
+
+
 
     }
 }
